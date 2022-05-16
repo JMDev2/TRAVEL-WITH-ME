c12a18012cc5e4f2bac27f0493faf53e5bdf8a26
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-16 20:25:30 +0300

    This will add the Sql2oPlaces Dao and impliments the interface Dao

--- src/main/java/Dao/Sql2oPlacesDao.java
+++ src/main/java/Dao/Sql2oPlacesDao.java
@@ -1,4 +1,27 @@
 package Dao;
 
-public class Sql2oPlacesDao {
+import models.Places;
+
+import java.util.List;
+
+public class Sql2oPlacesDao implements PlacesDao {
+    @Override
+    public void addPlace(Places places) {
+
+    }
+
+    @Override
+    public List<Places> getAllPlaces() {
+        return null;
+    }
+
+    @Override
+    public Places getAllPlacesById(int id) {
+        return null;
+    }
+
+    @Override
+    public void deletePlaceById(int id) {
+
+    }
 }
