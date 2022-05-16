bb3e0bae36a399c905890f77ccb7c6a9435de698
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-16 20:23:27 +0300

    this will add the places interface

--- src/main/java/Dao/PlacesDao.java
+++ src/main/java/Dao/PlacesDao.java
@@ -1,4 +1,23 @@
 package Dao;
 
+import models.Places;
+
+import java.util.List;
+
 public interface PlacesDao {
+//    CRUD
+//    Add place
+    void addPlace(Places places);
+
+//    READ
+    List<Places> getAllPlaces();
+    Places getAllPlacesById(int id);
+//    List<Reviews> getReviewsByPlace(int placeId);
+
+//    DELETE
+    void deletePlaceById(int id);
+
+
+
+
 }
