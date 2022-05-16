43f035dbc4754d0cc1604695fcc9066204d145a1
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-16 19:50:30 +0300

    This will add the class getters and setters

--- src/main/java/models/Places.java
+++ src/main/java/models/Places.java
@@ -14,4 +14,44 @@ public class Places {
         this.imageUrl = imageUrl;
         this.description = description;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
