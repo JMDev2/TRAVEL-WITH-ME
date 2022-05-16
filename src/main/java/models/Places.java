0e8ca065144ab0dd1dd06fa4aad8fb6f0768e52f
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-16 19:48:48 +0300

    This will add the class variable

--- src/main/java/models/Places.java
+++ src/main/java/models/Places.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Places {
+    private int id;
+    private String destination;
+    private String location;
+    private String imageUrl;
+    private String description;
+
+
+
 }
