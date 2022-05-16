39e8f3e20ee5fe1bba26c923a22608a48a2774fb
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-05-16 22:09:14 +0300

    This will add the add places method in the places Dao

--- src/main/java/Dao/Sql2oPlacesDao.java
+++ src/main/java/Dao/Sql2oPlacesDao.java
@@ -1,12 +1,31 @@
 package Dao;
 
 import models.Places;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
+
+import org.sql2o.Connection;
 import java.util.List;
 
 public class Sql2oPlacesDao implements PlacesDao {
+    private final Sql2o sql2o;
+    public Sql2oPlacesDao(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
     @Override
     public void addPlace(Places places) {
+        try(Connection conn = sql2o.open()){
+            String sql = "INSERT INTO places (destination, location, imageurl, description) VALUES (:destination, :location, :imageurl, :description)";
+            int id = (int) conn.createQuery(sql,true)
+                    .bind(places)
+                    .executeUpdate()
+                    .getKey();
+            places.setId(id);
+
+        }catch (Sql2oException ex){
+            System.out.println(ex);
+        }
 
 
 
